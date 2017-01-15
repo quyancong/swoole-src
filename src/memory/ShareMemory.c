eda6def9ef16d3c1a9509d26912d074074066944
曲延从 <quyancong@smzdm.com>
2017-01-16 00:01:01 +0800

    swShareMemory_mmap_create方法全注释

--- src/memory/ShareMemory.c
+++ src/memory/ShareMemory.c
@@ -89,53 +89,64 @@ void* sw_shm_realloc(void *ptr, size_t new_size)
     }
 }
 
-/*
- * mmap方式创建内存映射方式的共享内存
+/**
+ * mmap方式创建内存映射方式共享内存
+ * @param object    swShareMemory结构体变量指针
+ * @param size      要创建的内存映射的大小
+ * @param mapfile   内存要映射的文件的名字
+ * @return 
  */
 void *swShareMemory_mmap_create(swShareMemory *object, int size, char *mapfile)
 {
-    void *mem;
-    int tmpfd = -1;
-    int flag = MAP_SHARED;
-    bzero(object, sizeof(swShareMemory));
+    void *mem;  //指向任意变量类型的内存指针
+    int tmpfd = -1; //默认赋值-1.open函数，若所有欲核查的权限都通过了检查则返回 0 值，表示成功，只要有一个权限被禁止则返回-1
+    int flag = MAP_SHARED;  //mmap函数的 flag参数。MAP_SHARED 对映射区域的写入数据会复制回文件内，而且允许其他映射该文件的进程共享。
+    bzero(object, sizeof(swShareMemory)); //void bzero(void *s,int n);将参数s 所指的内存区域前n 个字节，全部设为零值。相当于调用memset(（void *）s,0,size_t n);推荐使用memset替代bzero。
 
 #ifdef MAP_ANONYMOUS
-    flag |= MAP_ANONYMOUS;
+    flag |= MAP_ANONYMOUS;  //flag = (flag | MAP_ANONYMOUS)   flag和 MAP_ANONYMOUS 进行二级制或运算 ???
 #else
+    //如果没有指定要映射的文件，则使用/dev/zero 文件。 /dev/zero和 /dev/null 是linux的两个特殊文件，不会保存写入内容
     if (mapfile == NULL)
     {
         mapfile = "/dev/zero";
     }
+    //读写方式打开要映射的文件，如果打开失败则 return NULL
     if ((tmpfd = open(mapfile, O_RDWR)) < 0)
     {
         return NULL;
     }
+    //将要映射的文件名赋值给swShareMemory结构体。c语言的字符串复制需要用strcpy和strncpy等，不能直接用=赋值
     strncpy(object->mapfile, mapfile, SW_SHM_MMAP_FILE_LEN);
+    //要映射的文件句柄赋值给swShareMemory结构体
     object->tmpfd = tmpfd;
 #endif
 
+//SW_USE_HUGEPAGE 在config.m4文件出现，应该是判断是否开启大内存页(默认内存一页是4KB，大页内存一页是2MB)
+//判断是否定义了MAP_HUGETLB(since Linux 2.6.32)，它作为mmap函数的参数，使用该参数可以在内存映射时开启大内存页，有利于提高TLB(Translation Lookaside Buffer)命中率，提高虚拟地址到物理地址的转换效率http://www.laruence.com/2015/10/02/3069.html
 #if defined(SW_USE_HUGEPAGE) && defined(MAP_HUGETLB)
+    //如果创建的内存映射大于2M则开启大内存页，因为大内存页正好一页2M
     if (size > 2 * 1024 * 1024)
     {
         flag |= MAP_HUGETLB;
     }
 #endif
-
+    //建立内存映射若映射成功则返回映射区的内存起始地址，否则返回MAP_FAILED（－1）。
     mem = mmap(NULL, size, PROT_READ | PROT_WRITE, flag, tmpfd, 0);
 #ifdef MAP_FAILED
     if (mem == MAP_FAILED)
 #else
     if (!mem)
 #endif
-    {
-        swWarn("mmap() failed. Error: %s[%d]", strerror(errno), errno);
+    {   //建立内存映射失败则输出错误内容和错误号
+        swWarn("mmap() failed. Error: %s[%d]", strerror(errno), errno);//swWarn定义未深入研究???
         return NULL;
     }
     else
-    {
+    {   //建立内存映射成功则给swShareMemory结构体赋值映射区内存大小和映射区内存起始地址
         object->size = size;
         object->mem = mem;
-        return mem;
+        return mem; //当前函数返回创建映射区的内存起始地址
     }
 }
 
